417ac42e667eeca9be9357ecae072076886ba5ac
Julian <cruzjulian.dev@gmail.com>
2023-08-22 06:34:05 -0300

    Subida Practico3

---
+++ Practico3/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Practico2
+{
+    partial class Formulario1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LNya = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LDni = new System.Windows.Forms.Label();
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.TDni = new System.Windows.Forms.TextBox();
+            this.TApellido = new System.Windows.Forms.TextBox();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.LModificar = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LNya
+            // 
+            this.LNya.AutoSize = true;
+            this.LNya.Location = new System.Drawing.Point(17, 13);
+            this.LNya.Name = "LNya";
+            this.LNya.Size = new System.Drawing.Size(95, 13);
+            this.LNya.TabIndex = 0;
+            this.LNya.Text = "Nombre y Apellido:";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(17, 207);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 1;
+            this.LNombre.Text = "Nombre";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(17, 143);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LDni
+            // 
+            this.LDni.AutoSize = true;
+            this.LDni.Location = new System.Drawing.Point(17, 79);
+            this.LDni.Name = "LDni";
+            this.LDni.Size = new System.Drawing.Size(26, 13);
+            this.LDni.TabIndex = 3;
+            this.LDni.Text = "DNI";
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.Location = new System.Drawing.Point(57, 444);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BGuardar.TabIndex = 4;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = true;
+            this.BGuardar.Click += new System.EventHandler(this.BGuardar_Click);
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.Location = new System.Drawing.Point(152, 444);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(75, 23);
+            this.BEliminar.TabIndex = 5;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.UseVisualStyleBackColor = true;
+            this.BEliminar.Click += new System.EventHandler(this.BEliminar_Click);
+            // 
+            // TDni
+            // 
+            this.TDni.Location = new System.Drawing.Point(83, 76);
+            this.TDni.Name = "TDni";
+            this.TDni.Size = new System.Drawing.Size(124, 20);
+            this.TDni.TabIndex = 6;
+            this.TDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TDni_KeyPress);
+            // 
+            // TApellido
+            // 
+            this.TApellido.Location = new System.Drawing.Point(83, 140);
+            this.TApellido.Name = "TApellido";
+            this.TApellido.Size = new System.Drawing.Size(124, 20);
+            this.TApellido.TabIndex = 7;
+            this.TApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TApellido_KeyPress);
+            // 
+            // TNombre
+            // 
+            this.TNombre.Location = new System.Drawing.Point(83, 204);
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(124, 20);
+            this.TNombre.TabIndex = 8;
+            this.TNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TNombre_KeyPress);
+            // 
+            // LModificar
+            // 
+            this.LModificar.AutoSize = true;
+            this.LModificar.BackColor = System.Drawing.SystemColors.Control;
+            this.LModificar.ForeColor = System.Drawing.Color.Crimson;
+            this.LModificar.Location = new System.Drawing.Point(133, 13);
+            this.LModificar.Name = "LModificar";
+            this.LModificar.Size = new System.Drawing.Size(50, 13);
+            this.LModificar.TabIndex = 9;
+            this.LModificar.Text = "Modificar";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.LModificar);
+            this.panel1.Controls.Add(this.TNombre);
+            this.panel1.Controls.Add(this.TApellido);
+            this.panel1.Controls.Add(this.TDni);
+            this.panel1.Controls.Add(this.LDni);
+            this.panel1.Controls.Add(this.LApellido);
+            this.panel1.Controls.Add(this.LNombre);
+            this.panel1.Controls.Add(this.LNya);
+            this.panel1.Location = new System.Drawing.Point(20, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(242, 362);
+            this.panel1.TabIndex = 10;
+            // 
+            // Formulario1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 540);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
+            this.Name = "Formulario1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pequeño Formulario";
+            this.Load += new System.EventHandler(this.Formulario1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LNya;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LDni;
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button BEliminar;
+        private System.Windows.Forms.TextBox TDni;
+        private System.Windows.Forms.TextBox TApellido;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.Label LModificar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Practico3/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Practico2
+{
+    public partial class Formulario1 : Form
+    {
+        public Formulario1()
+        {
+            InitializeComponent();
+        }
+
+        private void Formulario1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TDni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void BGuardar_Click(object sender, EventArgs e)
+        {
+
+            //if (string.IsNullOrEmpty(TNombre.Text.Trim()) || string.IsNullOrEmpty(TApellido.Text.Trim()) )
+            if (TDni.Text.Trim() == "" || TNombre.Text.Trim() == "" || TApellido.Text.Trim() == "")
+            {
+                MessageBox.Show("Debe completar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                LModificar.Text = TNombre.Text.Trim() + " " + TApellido.Text.Trim();
+                DialogResult ask = MessageBox.Show("Seguro que desea insertar un nuevo Cliente?", "Confirmar inserción", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (ask == DialogResult.Yes)
+                {
+                    MessageBox.Show("El Cliente: " + TNombre.Text.Trim() + " " + TApellido.Text.Trim() + " se insertó correctamente", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void BEliminar_Click(object sender, EventArgs e)
+        {
+            if (TDni.Text.Trim() == "" || TNombre.Text.Trim() == "" || TApellido.Text.Trim() == "")
+            {
+                MessageBox.Show("No existe cliente para eliminar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                DialogResult ask;
+                ask = MessageBox.Show("Está apunto de eliminar el Cliente: " + TNombre.Text.Trim() + " " + TApellido.Text.Trim(), "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+                if (ask == DialogResult.Yes)
+                {
+                    MessageBox.Show("El Cliente: " + TNombre.Text.Trim() + " " + TApellido.Text.Trim() + " se elimino correctamente", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    TDni.Clear();
+                    TNombre.Clear();
+                    TApellido.Clear();
+                    LModificar.Text = "Modificar";
+                }
+            }
+            
+        }
+    }
+}
