2b28a877c7b71276bd745ab77a2f9d224d17a84f
Julian <cruzjulian.dev@gmail.com>
2023-08-23 10:48:14 -0300

    agrega formulario MDI al iniciar

--- Practico3/Program.cs
+++ Practico3/Program.cs
@@ -16,7 +16,7 @@ namespace Practico2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Formulario1());
+            Application.Run(new MDIParent1());
         }
     }
 }
