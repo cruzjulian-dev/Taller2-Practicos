1e2d34c1116730bcb080397d0e893f5b1076013e
Julian <cruzjulian.dev@gmail.com>
2023-08-24 20:09:26 -0300

    Agrega labels, txt, y boton Generar funcional

--- Practico4/Form1.Designer.cs
+++ Practico4/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LDesde = new System.Windows.Forms.Label();
+            this.LHasta = new System.Windows.Forms.Label();
+            this.TDesde = new System.Windows.Forms.TextBox();
+            this.THasta = new System.Windows.Forms.TextBox();
+            this.BGenerar = new System.Windows.Forms.Button();
+            this.LLista = new System.Windows.Forms.Label();
+            this.LBLista = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // LDesde
+            // 
+            this.LDesde.AutoSize = true;
+            this.LDesde.Location = new System.Drawing.Point(45, 101);
+            this.LDesde.Name = "LDesde";
+            this.LDesde.Size = new System.Drawing.Size(38, 13);
+            this.LDesde.TabIndex = 0;
+            this.LDesde.Text = "Desde";
+            this.LDesde.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LHasta
+            // 
+            this.LHasta.AutoSize = true;
+            this.LHasta.Location = new System.Drawing.Point(48, 171);
+            this.LHasta.Name = "LHasta";
+            this.LHasta.Size = new System.Drawing.Size(35, 13);
+            this.LHasta.TabIndex = 1;
+            this.LHasta.Text = "Hasta";
+            // 
+            // TDesde
+            // 
+            this.TDesde.Location = new System.Drawing.Point(107, 98);
+            this.TDesde.Name = "TDesde";
+            this.TDesde.Size = new System.Drawing.Size(100, 20);
+            this.TDesde.TabIndex = 2;
+            // 
+            // THasta
+            // 
+            this.THasta.Location = new System.Drawing.Point(107, 168);
+            this.THasta.Name = "THasta";
+            this.THasta.Size = new System.Drawing.Size(100, 20);
+            this.THasta.TabIndex = 3;
+            // 
+            // BGenerar
+            // 
+            this.BGenerar.Location = new System.Drawing.Point(252, 96);
+            this.BGenerar.Name = "BGenerar";
+            this.BGenerar.Size = new System.Drawing.Size(118, 23);
+            this.BGenerar.TabIndex = 4;
+            this.BGenerar.Text = "Generar Funcion";
+            this.BGenerar.UseVisualStyleBackColor = true;
+            this.BGenerar.Click += new System.EventHandler(this.BGenerar_Click);
+            // 
+            // LLista
+            // 
+            this.LLista.AutoSize = true;
+            this.LLista.Location = new System.Drawing.Point(542, 70);
+            this.LLista.Name = "LLista";
+            this.LLista.Size = new System.Drawing.Size(89, 13);
+            this.LLista.TabIndex = 5;
+            this.LLista.Text = "Lista de Numeros";
+            // 
+            // LBLista
+            // 
+            this.LBLista.FormattingEnabled = true;
+            this.LBLista.Location = new System.Drawing.Point(410, 95);
+            this.LBLista.Name = "LBLista";
+            this.LBLista.Size = new System.Drawing.Size(358, 316);
+            this.LBLista.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LBLista);
+            this.Controls.Add(this.LLista);
+            this.Controls.Add(this.BGenerar);
+            this.Controls.Add(this.THasta);
+            this.Controls.Add(this.TDesde);
+            this.Controls.Add(this.LHasta);
+            this.Controls.Add(this.LDesde);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LDesde;
+        private System.Windows.Forms.Label LHasta;
+        private System.Windows.Forms.TextBox TDesde;
+        private System.Windows.Forms.TextBox THasta;
+        private System.Windows.Forms.Button BGenerar;
+        private System.Windows.Forms.Label LLista;
+        private System.Windows.Forms.ListBox LBLista;
     }
 }
 
--- Practico4/Form1.cs
+++ Practico4/Form1.cs
@@ -16,5 +16,57 @@ namespace Practico4
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BGenerar_Click(object sender, EventArgs e)
+        {
+            LBLista.Text = "";
+            if (TDesde.Text.Trim() != "" && THasta.Text.Trim() != "" )
+            {
+                int i;
+                char error = 'n';
+                for (i=0; i<THasta.TextLength; i++)
+                {
+                    if (!char.IsNumber(THasta.Text[i]))
+                    {
+                        error = 's';
+                        MessageBox.Show("Hay al menos un caracter no numérico", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                }
+
+                for (i = 0; i < TDesde.TextLength; i++)
+                {
+                    if (!char.IsNumber(TDesde.Text[i]))
+                    {
+                        error = 's';
+                        MessageBox.Show("Hay al menos un caracter no numérico", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                }
+
+                if (error != 's')
+                {
+                    int desde = int.Parse(TDesde.Text);
+                    int hasta = int.Parse(THasta.Text);
+                    
+                     while (desde <= hasta)
+                    { 
+                        LBLista.Items.Add(desde);
+                        desde++;
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Debes completar los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
     }
 }
