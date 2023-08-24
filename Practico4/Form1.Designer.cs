445a4598574b2ff5510d6717a7744250825a8947
Julian <cruzjulian.dev@gmail.com>
2023-08-24 20:24:24 -0300

    Agrega botones num pares, impares, primos

--- Practico4/Form1.Designer.cs
+++ Practico4/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.BGenerar = new System.Windows.Forms.Button();
             this.LLista = new System.Windows.Forms.Label();
             this.LBLista = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LDesde
@@ -74,7 +77,7 @@
             // 
             // BGenerar
             // 
-            this.BGenerar.Location = new System.Drawing.Point(252, 96);
+            this.BGenerar.Location = new System.Drawing.Point(89, 218);
             this.BGenerar.Name = "BGenerar";
             this.BGenerar.Size = new System.Drawing.Size(118, 23);
             this.BGenerar.TabIndex = 4;
@@ -85,7 +88,7 @@
             // LLista
             // 
             this.LLista.AutoSize = true;
-            this.LLista.Location = new System.Drawing.Point(542, 70);
+            this.LLista.Location = new System.Drawing.Point(406, 72);
             this.LLista.Name = "LLista";
             this.LLista.Size = new System.Drawing.Size(89, 13);
             this.LLista.TabIndex = 5;
@@ -94,16 +97,46 @@
             // LBLista
             // 
             this.LBLista.FormattingEnabled = true;
-            this.LBLista.Location = new System.Drawing.Point(410, 95);
+            this.LBLista.Location = new System.Drawing.Point(273, 98);
             this.LBLista.Name = "LBLista";
             this.LBLista.Size = new System.Drawing.Size(358, 316);
             this.LBLista.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Numeros Pares";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Numeros Impares";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(89, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Numeros Primos";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(655, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.LBLista);
             this.Controls.Add(this.LLista);
             this.Controls.Add(this.BGenerar);
@@ -127,6 +160,9 @@
         private System.Windows.Forms.Button BGenerar;
         private System.Windows.Forms.Label LLista;
         private System.Windows.Forms.ListBox LBLista;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
