f8c225bfa6f00747fbc1ea3c167689da131754c3
Julian <cruzjulian.dev@gmail.com>
2023-08-25 10:31:53 -0300

    Falta chart

--- Practico4/Form1.Designer.cs
+++ Practico4/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.LDesde = new System.Windows.Forms.Label();
             this.LHasta = new System.Windows.Forms.Label();
             this.TDesde = new System.Windows.Forms.TextBox();
@@ -94,9 +94,10 @@
             // LLista
             // 
             this.LLista.AutoSize = true;
-            this.LLista.Location = new System.Drawing.Point(382, 59);
+            this.LLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LLista.Location = new System.Drawing.Point(332, 25);
             this.LLista.Name = "LLista";
-            this.LLista.Size = new System.Drawing.Size(89, 13);
+            this.LLista.Size = new System.Drawing.Size(202, 29);
             this.LLista.TabIndex = 5;
             this.LLista.Text = "Lista de Numeros";
             // 
@@ -140,16 +141,16 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(633, 85);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(290, 300);
             this.chart1.TabIndex = 10;
             this.chart1.Text = "chart1";
